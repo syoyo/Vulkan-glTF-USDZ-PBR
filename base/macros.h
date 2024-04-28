f80aa48ef21b025571b80eaa67dec7e846432c69
Syoyo Fujita <syoyo@lighttransport.com>
2024-04-29 08:24:30 +0900

    Fix android build.

--- base/macros.h
+++ base/macros.h
@@ -11,6 +11,10 @@
 #include "vulkan/vulkan.h"
 
 #if defined(__ANDROID__)
+#include "VulkanAndroid.h"
+#endif
+
+#if defined(__ANDROID__)
 #define VK_CHECK_RESULT(f)																				\
 {																										\
 	VkResult res = (f);																					\
